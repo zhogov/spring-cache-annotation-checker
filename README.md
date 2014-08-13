16cbca003fc226e76af97ee5ea227d4a72a2f47b
Zhogov <azhogov@itcwin.com>
2014-08-13 13:04:56 +0400

    Readme

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 spring-cache-annotation-checker
 ===============================
 
-Compile-time annotation processor, intented to 
+Spring cache compile-time annotation processor
+
+Annotation processor intended to check "key", "condition" and "unless" fields of 
+org.springframework.cache.annotation.Cacheable,
+org.springframework.cache.annotation.CacheEvict, 
+org.springframework.cache.annotation.CachePut,
+org.springframework.cache.annotation.Caching
+annotations.
+
+Those fields have references to parameters of annotated method. If Spring cannot find corresponding method parameter
+it will not throw ny exception and not found parameters will be null. For example:
+
+@Cacheable(value = "AuthorCache", key = "#id_author", unless = "#result==null")
+public AuthorEntity getEntity(Integer id) {
+...
+}
+
+Here #id_author will be null, because SPEL evaluator will not be able to find it.
+Using this checker you will get compilation error pointing to getEntity method and #id_author variable.
+
--- src/main/java/org/zhogov/annotationchecker/SpringCacheAnnotationChecker.java
+++ src/main/java/org/zhogov/annotationchecker/SpringCacheAnnotationChecker.java
@@ -22,17 +22,26 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// Annotation processor intended to check "key", "condition" and "unless" fields of org.springframework.cache.annotation.Cacheable,
-// org.springframework.cache.annotation.CacheEvict, org.springframework.cache.annotation.CachePut annotations.
-//
-// Those fields have references to parameters of annotated method. If Spring cannot find corresponding method parameter
-// it will not throw ny exception and not found parameters will be null. For example:
-//
-// @Cacheable(value = "AuthorCache", key = "#id_author", unless = "#result==null")
-// public AuthorEntity getEntity(Integer id)
-//
-// Here #id_author will be null, because SPEL evaluator will not be able to find it.
-// Using this checker you will get compilation error pointing to getEntity method and #id_author variable.g
+/*
+Annotation processor intended to check "key", "condition" and "unless" fields of
+org.springframework.cache.annotation.Cacheable,
+org.springframework.cache.annotation.CacheEvict,
+org.springframework.cache.annotation.CachePut,
+org.springframework.cache.annotation.Caching
+annotations.
+
+Those fields have references to parameters of annotated method. If Spring cannot find corresponding method parameter
+it will not throw ny exception and not found parameters will be null. For example:
+
+@Cacheable(value = "AuthorCache", key = "#id_author", unless = "#result==null")
+public AuthorEntity getEntity(Integer id) {
+...
+}
+
+Here #id_author will be null, because SPEL evaluator will not be able to find it.
+Using this checker you will get compilation error pointing to getEntity method and #id_author variable.
+*/
+
 @SupportedAnnotationTypes({"org.springframework.cache.annotation.Cacheable",
         "org.springframework.cache.annotation.CacheEvict", "org.springframework.cache.annotation.CachePut",
         "org.springframework.cache.annotation.Caching"})
